bc8c7c52cd00d1b70ea860d0a08d0a3f37c91afb
lihu <tinylihu@163.com>
2019-12-09 20:42:15 +0800

    抽屉

--- src/layouts/Drawer/index.js
+++ src/layouts/Drawer/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
-import { Drawer, Button, Icon, Row, Col } from 'antd'
+import { Drawer, Row, Switch } from 'antd'
 import { inject, observer } from 'mobx-react'
-
+import style from './index.module.less'
 
 const DrawerWrapper = inject('store')(observer((props) =>{
     let {store} = props
@@ -11,12 +11,31 @@ const DrawerWrapper = inject('store')(observer((props) =>{
             <div>
                 <Drawer
                     title="设置"
-                    width={720}
+                    width={380}
                     onClose={store.setDrawer}
                     visible={store.drawer}
                     bodyStyle={{ paddingBottom: 80 }}
+                    className={style.drawer}
                 >
-                    
+                    <Row>
+                        <div className={style.column}>
+                            <div className={style.title}>主题</div>
+                            <div><Switch defaultChecked /></div>
+                        </div>
+                        <div className={style.row}>
+                            <div>切换模式</div>
+                            <div><Switch defaultChecked onClick={()=>{ store.setDark()}} /></div>
+                        </div>
+                        
+                        {/* <Col span={24} className={style.title}>主题</Col>
+                        <Col span={24}>
+                            s
+                        </Col>
+                        <Row>
+                            <Col span={12}><span>切换模式</span> </Col>
+                            <Col span={12} push={8}><Switch defaultChecked /></Col>
+                        </Row> */}
+                    </Row>
                 </Drawer>
             </div>
         </>
